7f7f8d50d8dd7f78e189cb6c498c13bae848ecdc
Nero Argento <drakonobor@gmail.com>
2023-12-06 22:49:44 +0300

    search feature

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -27,3 +27,22 @@ def entry(request, title):
             "entry_title": title,
             "content": html_content
         })
+    
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+            "entry_title": entry_search,
+            "content": html_content
+        })
+        else:
+            allEntries = util.list_entries()
+            recommendation = []
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "recommendation": recommendation
+            })
